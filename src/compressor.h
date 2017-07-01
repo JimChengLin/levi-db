4dad61ff532ed97d2bf1d30e12bac06a7d472634
yuanjinlin <jimzuolin@gmail.com>
2017-07-02 00:38:46 +0800

    Compressor设定

--- src/compressor.h
+++ src/compressor.h
@@ -6,7 +6,18 @@
  * 1. 重复字符串检测器(repeat_detector)
  * 2. 算术编码器(coder)
  *
- * 长度缩短12.5%以上才接受压缩后的编码
+ * 长度缩短12.5%以上才接受压缩后的编码,
+ * 否则返回 原编码 + checksum
+ *
+ * 调用步骤:
+ * 提交源字符串, 再不断申请片段,
+ * 之间必须回填上段实际写入的位置
  */
+
+namespace LeviDB {
+    class Compressor {
+
+    };
+}
 
 #endif //LEVIDB_COMPRESSOR_H
