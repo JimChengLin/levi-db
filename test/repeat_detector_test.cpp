19d251cbd9c6174e8d43513e1e6150fadd818b3a
yuanjinlin <jimzuolin@gmail.com>
2017-07-07 12:38:41 +0800

    完成repeat_detector测试

--- test/repeat_detector_test.cpp
+++ test/repeat_detector_test.cpp
@@ -1,23 +1,20 @@
 #include "../src/repeat_detector.h"
+#include <array>
+#include <iostream>
 
 void repeat_detector_test() {
-    using namespace LeviDB;
-
-    class Tester : public SuffixTree {
-    private:
-        Arena arena;
-
+    class Tester : public LeviDB::SuffixTree {
     public:
-        Tester() noexcept : arena(), SuffixTree(&arena) {}
+        Tester(LeviDB::Arena * arena) noexcept : SuffixTree(arena) {}
 
-        bool contains(std::string & src, int from) {
-            const STNode * edge_node = nodeGetSub(_root, char_be_uint8(src[from]));
+        bool contains(std::string & src, int from) const noexcept {
+            const LeviDB::STNode * edge_node = nodeGetSub(_root, LeviDB::char_be_uint8(src[from]));
             if (edge_node == nullptr) {
                 return false;
             }
 
             while (true) {
-                const Slice edge_s = _chunk[edge_node->chunk_idx];
+                const LeviDB::Slice edge_s = _chunk[edge_node->chunk_idx];
                 for (int i = edge_node->from; i < edge_node->to; ++i) {
                     if (edge_s.data()[i] != src[from]) {
                         return false;
@@ -28,25 +25,33 @@ void repeat_detector_test() {
                         }
                     }
                 }
-                edge_node = nodeGetSub(edge_node, char_be_uint8(src[from]));
+                edge_node = nodeGetSub(edge_node, LeviDB::char_be_uint8(src[from]));
             }
         }
     };
 
-    Tester tester;
+    LeviDB::Arena arena;
+    Tester tester(&arena);
+
     srand(19950207);
-    char alphabet[] = {'A', 'B', 'C', 'D', 'E'};
-    for (int i = 0; i < 100; ++i) {
+    static constexpr int n = 100;
+    std::array<std::string, n> sources;
+    std::array<char, 5> alphabet = {'A', 'B', 'C', 'D', 'E'};
+
+    for (int i = 0; i < n; ++i) {
         std::string sample;
         int len = rand() % 20 + 1;
         for (int j = 0; j < len; ++j) {
-            sample += alphabet[rand() % 5];
+            sample += alphabet[rand() % alphabet.size()];
         }
         sample += '.';
+        sources[i] = std::move(sample);
 
-        tester.setitem(sample);
+        tester.setitem(LeviDB::Slice(sources[i]));
         for (int j = 0; j < sample.size(); ++j) {
             assert(tester.contains(sample, j));
         }
     }
+
+    std::cout << __FUNCTION__ << std::endl;
 };
