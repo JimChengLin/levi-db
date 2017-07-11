ee71e08d1755c38dfe0d694e1aa6c40384072b8d
yuanjinlin <jimzuolin@gmail.com>
2017-07-11 14:23:16 +0800

    修正find best pos

--- src/index.h
+++ src/index.h
@@ -13,6 +13,7 @@
 #include <cassert>
 #include <cstdint>
 #include <type_traits>
+#include <utility>
 
 namespace LeviDB {
     namespace IndexConst {
@@ -22,12 +23,12 @@ namespace LeviDB {
     template<typename T>
     class BDNode {
     public:
-        class CritPointer {
+        class CritPtr {
         private:
             void * _ptr;
 
         public:
-            CritPointer() noexcept : _ptr(nullptr) {}
+            CritPtr() noexcept : _ptr(nullptr) {}
 
             bool isNull() const noexcept {
                 return _ptr == nullptr;
@@ -61,7 +62,7 @@ namespace LeviDB {
                 return reinterpret_cast<BDNode *>(reinterpret_cast<uintptr_t>(this) & (UINTPTR_MAX - 1));
             }
 
-            ~CritPointer() noexcept {
+            ~CritPtr() noexcept {
                 if (!isNull()) {
                     if (isVal()) {
                         delete asVal();
@@ -72,17 +73,17 @@ namespace LeviDB {
             }
         };
 
-        std::array<CritPointer, IndexConst::rank + 1> _ptrs;
+        std::array<CritPtr, IndexConst::rank + 1> _ptrs;
         std::array<uint32_t, IndexConst::rank> _diffs;
         std::array<uint8_t, IndexConst::rank> _masks;
 
-        static_assert(std::is_standard_layout<decltype(_ptrs)>::value, "layout error");
-        static_assert(std::is_standard_layout<decltype(_diffs)>::value, "layout error");
-        static_assert(std::is_standard_layout<decltype(_masks)>::value, "layout error");
+        static_assert(std::is_standard_layout<decltype(_ptrs)>::value, "standard layout for mmap");
+        static_assert(std::is_standard_layout<decltype(_diffs)>::value, "standard layout for mmap");
+        static_assert(std::is_standard_layout<decltype(_masks)>::value, "standard layout for mmap");
 
     public:
         BDNode() noexcept : _ptrs(), _diffs(), _masks() {
-            static_assert(std::is_standard_layout<decltype(this)>::value, "layout error");
+            static_assert(std::is_standard_layout<decltype(this)>::value, "standard layout for mmap");
         }
 
         ~BDNode() noexcept {}
@@ -106,46 +107,51 @@ namespace LeviDB {
         }
 
     private:
-        BDNode<T> * findBestNode(const Slice & key) const noexcept {
-            BDNode<T> * cursor = _root;
-            while (cursor->full()) {
-
-            }
-            return cursor;
-        }
-
-        size_t findBestPos(BDNode<T> * node, const Slice & key) const noexcept {
+        std::pair<size_t, bool> findBestPos(BDNode<T> * node, const Slice & key) const noexcept {
             const uint32_t * cbegin = node->_diffs.cbegin();
-            const uint32_t * cend = node->_diffs.cend();
+            const uint32_t * cend;
 
-            while (true) {
-                if (cbegin == cend) {
+            int ptr_cnt = 0;
+            for (BDNode<T>::CritPtr ptr:node->_ptrs) {
+                if (!ptr.isNull()) {
+                    ++ptr_cnt;
+                } else {
                     break;
                 }
+            }
+            if (ptr_cnt <= 1) {
+                return {0, false};
+            } else {
+                cend = &node->_diffs[ptr_cnt - 1];
+            }
 
-                auto min_max = std::minmax_element(cbegin, cend);
-                const uint32_t * min_it = min_max.first;
-                const uint32_t * max_it = min_max.second;
-                const uint32_t diff_at = *min_it;
+            while (true) {
+                const uint32_t * min_it = std::min_element(cbegin, cend,
+                                                           [&node](const uint32_t & a, const uint32_t & b) {
+                                                               if (a < b) {
+                                                                   return true;
+                                                               } else if (a == b) {
+                                                                   return node->_masks[node->_diffs.cbegin() - &a] <
+                                                                          node->_masks[node->_diffs.cbegin() - &b];
+                                                               } else {
+                                                                   return false;
+                                                               }
+                                                           });
+                uint32_t diff_at = *min_it;
 
                 // go left or right
                 uint8_t crit_byte = key.size() > diff_at ? char_be_uint8(key[diff_at]) : static_cast<uint8_t>(0);
-                bool direct = (static_cast<uint8_t>(1) + (node->_masks[diff_at] | crit_byte)) >> 8;
-                if (direct == 0) { // left
+                bool false_left_true_right = (static_cast<uint8_t>(1) + (node->_masks[diff_at] | crit_byte)) >> 8;
+                if (!false_left_true_right) {
                     cend = min_it;
-                    if (max_it <= min_it) {
-                        cbegin = max_it;
-                    }
-                } else { // right
-                    assert(direct == 1);
+                } else {
                     cbegin = min_it + 1;
-                    if (max_it >= min_it) {
-                        cend = max_it + 1;
-                    }
+                }
+
+                if (cbegin == cend) {
+                    return {node->_diffs.cbegin() - min_it, false_left_true_right};
                 }
             }
-
-            return node->_diffs.cbegin() - cbegin;
         }
     };
 }
