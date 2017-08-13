4a1ac3cb4c5b29375323b0b2b74a88f553802dcf
yuanjinlin <jimzuolin@gmail.com>
2017-08-14 00:52:12 +0800

    add seq_gen

--- src/seq_gen.cpp
+++ src/seq_gen.cpp
@@ -1,1 +1,31 @@
+#include <cassert>
 
+#include "seq_gen.h"
+
+namespace LeviDB {
+    Snapshot::Snapshot(uint64_t seq_num, Snapshot * dummy_head) noexcept: _seq_num(seq_num) {
+        _next = dummy_head;
+        _prev = dummy_head->_prev;
+        _prev->_next = this;
+        _next->_prev = this;
+    }
+
+    Snapshot::~Snapshot() noexcept {
+        _prev->_next = _next;
+        _next->_prev = _prev;
+    }
+
+    std::unique_ptr<Snapshot> SeqGenerator::makeSnapshot() noexcept {
+        return std::make_unique<Snapshot>(uniqueSeqAtomic(), &_dummy_head);
+    }
+
+    uint64_t SeqGenerator::oldest() const noexcept {
+        assert(!empty());
+        return _dummy_head._next->immut_seq_num();
+    }
+
+    uint64_t SeqGenerator::newest() const noexcept {
+        assert(!empty());
+        return _dummy_head._prev->immut_seq_num();
+    }
+}
--- src/seq_gen.h
+++ src/seq_gen.h
@@ -2,7 +2,59 @@
 #define LEVIDB_SEQ_GEN_H
 
 /*
- * 生成全局唯一序列号
+ * 生成全局唯一序列号并管理快照
  */
+
+#include <atomic>
+#include <cassert>
+#include <cstdint>
+#include <memory>
+
+#include "util.h"
+
+namespace LeviDB {
+    /*
+     * doubly-linked list
+     *    |------------------|
+     *   old <-> new <-> head|
+     */
+    class Snapshot {
+    private:
+        Snapshot * _prev;
+        Snapshot * _next;
+        const uint64_t _seq_num = 0;
+
+        friend class SeqGenerator;
+
+    public:
+        Snapshot() noexcept : _prev(this), _next(this) {}
+
+        Snapshot(uint64_t seq_num, Snapshot * dummy_head) noexcept;
+
+        ~Snapshot() noexcept;
+
+        uint64_t immut_seq_num() const noexcept { return _seq_num; }
+
+        DELETE_MOVE(Snapshot);
+        DELETE_COPY(Snapshot);
+    };
+
+    class SeqGenerator {
+    private:
+        Snapshot _dummy_head;
+        std::atomic<std::uint64_t> _seq{0};
+
+    public:
+        uint64_t uniqueSeqAtomic() noexcept { return _seq.fetch_add(1); }
+
+        std::unique_ptr<Snapshot> makeSnapshot() noexcept;
+
+        bool empty() const noexcept { return _dummy_head._next == &_dummy_head; }
+
+        uint64_t oldest() const noexcept;
+
+        uint64_t newest() const noexcept;
+    };
+}
 
 #endif //LEVIDB_SEQ_GEN_H
