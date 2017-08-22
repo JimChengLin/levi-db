403e9dec56acdc23f380e858d92e6d848bf166ce
yuanjinlin <jimzuolin@gmail.com>
2017-08-22 13:27:47 +0800

    addback varint

--- src/varint.h
+++ src/varint.h
@@ -1,4 +1,17 @@
 #ifndef LEVIDB_VARINT_H
 #define LEVIDB_VARINT_H
 
+/*
+ * 变长 uint
+ */
+
+#include <cstdint>
+
+namespace LeviDB {
+    char * encodeVarint32(char * dst, uint32_t value) noexcept;
+
+    const char * // nullptr if fail
+    decodeVarint32(const char * p, const char * limit, uint32_t * value) noexcept;
+}
+
 #endif //LEVIDB_VARINT_H
