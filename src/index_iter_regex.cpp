27cea738358f75713a53456543f6bb364146f622
yuanjinlin <jimzuolin@gmail.com>
2017-08-31 14:38:05 +0800

    index iter add

--- src/index_iter_regex.cpp
+++ src/index_iter_regex.cpp
@@ -1,1 +1,12 @@
+#include "index_iter_regex.h"
 
+namespace LeviDB {
+    std::unique_ptr<Iterator<Slice, std::string>>
+    IndexIter::makeIterator() const noexcept {
+
+    }
+
+    void IndexIter::tryApplyPending() {
+
+    }
+}
