12a33bdb5f4ad5ee0fdabf31fc04096a78a6b2f6
yuanjinlin <jimzuolin@gmail.com>
2017-09-13 14:53:54 +0800

    db interface

--- src/db.h
+++ src/db.h
@@ -1,4 +1,59 @@
 #ifndef LEVIDB_DB_H
 #define LEVIDB_DB_H
 
+/*
+ * DB 接口
+ * 要求:
+ * 1. 实现增删改, 随机迭代器, (正向|反向)正则匹配迭代器
+ * 2. 线程安全
+ * 3. 妥善处理异常, 不需要额外操作进行数据恢复
+ */
+
+#include <string>
+
+#include "iterator.h"
+#include "levi_regex/r.h"
+#include "options.h"
+#include "slice.h"
+
+namespace LeviDB {
+    class DB {
+    protected:
+        std::string _name;
+        Options _options;
+
+    public:
+        DB(std::string name, Options options) noexcept : _name(std::move(name)), _options(options) {};
+        DEFAULT_MOVE(DB);
+        DEFAULT_COPY(DB);
+
+    public:
+        virtual ~DB() noexcept = default;
+
+        virtual void put(const WriteOptions & options,
+                         const Slice & key,
+                         const Slice & value) = 0;
+
+        virtual void remove(const WriteOptions & options,
+                            const Slice & key) = 0;
+
+        virtual void write(const WriteOptions & options,
+                           const std::vector<std::pair<Slice, Slice>> & kvs) = 0;
+
+        virtual std::string get(const ReadOptions & options,
+                                const Slice & key) const = 0;
+
+        virtual std::unique_ptr<Snapshot> makeSnapshot() = 0;
+
+        virtual std::unique_ptr<Iterator<Slice, std::string>>
+        makeIterator(std::unique_ptr<Snapshot> && snapshot) const = 0;
+
+        virtual std::unique_ptr<SimpleIterator<std::pair<Slice, std::string>>>
+        makeRegexIterator(std::shared_ptr<Regex::R> regex, std::unique_ptr<Snapshot> && snapshot) const = 0;
+
+        virtual std::unique_ptr<SimpleIterator<std::pair<Slice, std::string>>>
+        makeRegexReversedIterator(std::shared_ptr<Regex::R> regex, std::unique_ptr<Snapshot> && snapshot) const = 0;
+    };
+}
+
 #endif //LEVIDB_DB_H
--- src/options.h
+++ src/options.h
@@ -24,6 +24,7 @@ namespace LeviDB {
 
     struct WriteOptions {
         bool sync = false;
+        bool compress = false;
     };
 }
 
