9576e2a71cbba2c525d4281d3d14c8a165cf33f4
yuanjinlin <jimzuolin@gmail.com>
2017-11-29 01:39:24 +0800

    no need to bench with leveldb

--- bench/db_bench.cpp
+++ bench/db_bench.cpp
@@ -2,7 +2,6 @@
 
 #include <array>
 #include <iostream>
-#include <leveldb/db.h>
 #include <rocksdb/db.h>
 #include <thread>
 
@@ -112,40 +111,6 @@ void rocksdb_write_bench() {
     delete db;
 }
 
-void leveldb_write_bench() {
-    const std::string db_name = "/tmp/level_bench_db";
-    if (levidb8::env_io::fileExists(db_name)) {
-        leveldb::DestroyDB(db_name, {});
-    }
-    assert(!levidb8::env_io::fileExists(db_name));
-
-    leveldb::DB * db;
-    leveldb::Options options{};
-    options.create_if_missing = true;
-    auto s = leveldb::DB::Open(options, db_name, &db);
-    assert(s.ok());
-
-    std::vector<std::thread> jobs;
-    for (size_t i = 0; i < thread_num; ++i) {
-        jobs.emplace_back([db](size_t n) {
-            Src src;
-            for (size_t j = 0; j < test_times; ++j) {
-                if (j % thread_num == n) {
-                    auto item = src.readItem();
-                    db->Put({}, item.first, item.second);
-                } else {
-                    src.skipItem();
-                }
-            }
-        }, i);
-    }
-
-    for (auto & job:jobs) {
-        job.join();
-    }
-    delete db;
-}
-
 void levidb_read_bench() {
     const std::string db_name = "/tmp/levi_bench_db";
     auto db = levidb8::DB::open(db_name, {});
@@ -175,35 +140,6 @@ void rocksdb_read_bench() {
     const std::string db_name = "/tmp/rocks_bench_db";
     rocksdb::DB * db;
     auto s = rocksdb::DB::Open({}, db_name, &db);
-    assert(s.ok());
-
-    std::vector<std::thread> jobs;
-    for (size_t i = 0; i < thread_num; ++i) {
-        jobs.emplace_back([db](size_t n) {
-            Src src;
-            for (size_t j = 0; j < test_times; ++j) {
-                if (j % thread_num == n) {
-                    auto item = src.readItem();
-                    std::string res;
-                    db->Get({}, item.first, &res);
-                    assert(res == item.second);
-                } else {
-                    src.skipItem();
-                }
-            }
-        }, i);
-    }
-
-    for (auto & job:jobs) {
-        job.join();
-    }
-    delete db;
-}
-
-void leveldb_read_bench() {
-    const std::string db_name = "/tmp/level_bench_db";
-    leveldb::DB * db;
-    auto s = leveldb::DB::Open({}, db_name, &db);
     assert(s.ok());
 
     std::vector<std::thread> jobs;
@@ -261,24 +197,6 @@ void rocksdb_scan_bench() {
     std::cout << "rocksdb scan " << cnt << std::endl;
 }
 
-void leveldb_scan_bench() {
-    const std::string db_name = "/tmp/level_bench_db";
-    leveldb::DB * db;
-    auto s = leveldb::DB::Open({}, db_name, &db);
-    assert(s.ok());
-
-    auto it = db->NewIterator({});
-    size_t cnt = 0;
-    for (it->SeekToFirst();
-         it->Valid();
-         it->Next()) {
-        cnt += it->key().size() + it->value().size();
-    }
-    delete it;
-    delete db;
-    std::cout << "rocksdb scan " << cnt << std::endl;
-}
-
 using ms = std::chrono::milliseconds;
 
 #define PRINT_TIME(name) \
@@ -294,11 +212,6 @@ void write_bench() {
     rocksdb_write_bench();
     end = std::chrono::high_resolution_clock::now();
     PRINT_TIME(rocksdb_write_bench);
-
-    start = std::chrono::high_resolution_clock::now();
-    leveldb_write_bench();
-    end = std::chrono::high_resolution_clock::now();
-    PRINT_TIME(leveldb_write_bench);
 }
 
 void read_bench() {
@@ -311,11 +224,6 @@ void read_bench() {
     rocksdb_read_bench();
     end = std::chrono::high_resolution_clock::now();
     PRINT_TIME(rocksdb_read_bench);
-
-    start = std::chrono::high_resolution_clock::now();
-    leveldb_read_bench();
-    end = std::chrono::high_resolution_clock::now();
-    PRINT_TIME(leveldb_read_bench);
 }
 
 void scan_bench() {
@@ -328,11 +236,6 @@ void scan_bench() {
     rocksdb_scan_bench();
     end = std::chrono::high_resolution_clock::now();
     PRINT_TIME(rocksdb_scan_bench);
-
-    start = std::chrono::high_resolution_clock::now();
-    leveldb_scan_bench();
-    end = std::chrono::high_resolution_clock::now();
-    PRINT_TIME(leveldb_scan_bench);
 }
 
 void db_bench() {
